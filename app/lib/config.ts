c13b31d5f32ed930396149ec8fe3fed9170e5af6
Nazarov Kanat <kanatnazarov.dev@gmail.com>
2026-02-05 11:40:02 -0500

    added membership payload 2

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -4,5 +4,9 @@ export const FORM_ROUTING = {
     webhookUrl: "https://hooks.newo.ai/BpK3NhYODUjJcnK3mz77pQ",
     scenario: "Emergency Dental"
   },
+   "membership_form_id": {
+    webhookUrl: "https://hooks.newo.ai/BpK3NhYODUjJcnK3mz77pQ",
+    scenario: "Membership Dental"
+  },
 
 };
